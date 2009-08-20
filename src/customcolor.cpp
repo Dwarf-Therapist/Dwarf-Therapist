b84e1b3e88090a025fbc4ae194461dbd6e729099
Trey Stout <treystout@gmail.com>
2009-08-20 18:01:59 +0000

    hacking together some linux fun!

--- src/customcolor.cpp
+++ src/customcolor.cpp
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 #include <QtGui>
-#include <qtcolorpicker.h>
+#include <QtColorPicker>
 
 #include "customcolor.h"
 
