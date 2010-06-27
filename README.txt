5bbe236e2e3c1638a2cf2b3d2198dc01be2698d3
Trey Stout <treystout@gmail.com>
2010-06-26 22:47:21 -0700

    * Prep for release of 0.5.5

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-Dwarf Therapist v0.5.4
+Dwarf Therapist v0.5.5
 Copyright (c) 2009,2010 Trey Stout (chmod)
 Homepage: http://code.google.com/p/dwarftherapist/
 License: MIT License (see included LICENSE.txt file)
--- google_app/versionhandler.py
+++ google_app/versionhandler.py
@@ -5,7 +5,7 @@ from google.appengine.ext import webapp
 from metricentry import MetricEntry
 from utils import ip_info
 
-CURRENT_VERSION = "0.5.4"
+CURRENT_VERSION = "0.5.5"
 RX_VERSION_CHUNKER = re.compile(r"\s*DwarfTherapist\s+(\d+)\.(\d+)\.(\d+)\s*")
 class VersionHandler(webapp.RequestHandler):
     def get(self, *args):
