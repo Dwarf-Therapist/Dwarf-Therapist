0a496f320274a31a791aa7f966933995c37d216c
joshb <devnull@localhost>
2013-10-10 14:14:28 +0200

    v20.6.3 cont. - forgot osx makedist.sh

---
+++ osx/makedist.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+rm -rf ../../dist
+mkdir -p ../../dist
+hdiutil convert template.dmg -format UDSP -o ../../dist/DwarfTherapist
+hdiutil resize -size 80m ../../dist/DwarfTherapist.sparseimage
+hdiutil mount ../../dist/DwarfTherapist.sparseimage
+
+# Prepare contents
+cp -R ../bin/release/DwarfTherapist.app/Contents /Volumes/Dwarf\ Therapist/DwarfTherapist.app/Contents
+cp -f ../CHANGELOG.txt /Volumes/Dwarf\ Therapist/
+cp -f ../LICENSE.txt /Volumes/Dwarf\ Therapist/
+cp -f ../README.txt /Volumes/Dwarf\ Therapist/
+rm -f /Volumes/Dwarf\ Therapist/DwarfTherapist.app/Contents/MacOS/log/run.log
+
+macdeployqt /Volumes/Dwarf\ Therapist/DwarfTherapist.app -no-plugins
+
+hdiutil eject /volumes/Dwarf\ Therapist/
+hdiutil convert ../../dist/DwarfTherapist.sparseimage -format UDBZ -o ../../dist/DwarfTherapist.dmg
+rm -f ../../dist/DwarfTherapist.sparseimage
+
