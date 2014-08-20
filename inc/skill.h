0263f8b01c962ceb9404024bc294e943e11e6412
Alex Xu <alex_y_xu@yahoo.ca>
2014-08-20 16:41:48 -0400

    Remove unused declarations, silence warning.

--- inc/skill.h
+++ inc/skill.h
@@ -89,7 +89,6 @@ private:
     short m_capped_level;
     short m_raw_level;
     QString m_name;
-    int m_demotions;    
     QColor m_rust_color;
     QString m_rust_rating;
     int m_skill_rate;
