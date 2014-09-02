d659d6aa9cf8e6bb8499c5ec344cdbfcc2f8f12b
Alex Xu <alex_y_xu@yahoo.ca>
2014-09-02 13:55:50 -0400

    Fix file finding. (fixes #158)

--- inc/utils.h
+++ inc/utils.h
@@ -195,15 +195,18 @@ static inline QString nice_list(QStringList values){
     return ret_val;
 }
 
-static inline QStringList find_files_list(const QString &subdir, const QString &file){
+static inline QStringList find_files_list(const QString &file){
     QStringList out;
     QString working_dir = QDir::current().path();
     QString appdir = QCoreApplication::applicationDirPath();
-    out << QString("%1/%2/%3").arg(appdir, subdir, file);
-    out << QString("%1/../%2/%3").arg(appdir, subdir, file);
+    // Dwarf Therapist xx.x/share/game_data.ini
+    out << QString("%1/share/%2").arg(appdir, file);
+    // Dwarf-Therapist/release/../share/game_data.ini
+    out << QString("%1/../share/%2").arg(appdir, file);
+    // /usr/bin/../share/dwarftherapist/game_data.ini
+    out << QString("%1/../share/dwarftherapist/%2").arg(appdir, file);
+    // cwd/game_data.ini
     out << QString("%1/%2").arg(working_dir, file);
-    out << QString("%1/../%2/%3").arg(working_dir, subdir, file);
-    out << QString("%1/%2/%3").arg(working_dir, subdir, file);
     return out;
 }
 
