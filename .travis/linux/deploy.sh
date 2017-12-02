6dd1feb8fe98f93b4e7c20c18f07d8110d0ba161
Clément Vuchener <clement.vuchener@gmail.com>
2017-12-02 12:08:21 +0100

    travis: deploy linux AppImage and osx dmg

---
+++ .travis/linux/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ninja install
+
+# Add PDF manual
+wget "http://dffd.bay12games.com/download.php?id=7889&f=Dwarf+Therapist.pdf" -O "$DT_PREFIX/share/doc/Dwarf Therapist.pdf"
+
+# Download linuxdeployqt
+wget "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+linuxdeployqt="./linuxdeployqt-continuous-x86_64.AppImage"
+chmod +x "$linuxdeployqt"
+
+# does not work because of missing FUSE
+#"$linuxdeployqt" "$PREFIX/usr/share/applications/dwarftherapist.desktop" -appimage
+
+# extract AppImage content instead
+"$linuxdeployqt" --appimage-extract
+EXTRACTED_PREFIX=$PWD/squashfs-root/usr
+export PATH=$EXTRACTED_PREFIX/bin:$PATH
+export LD_LIBRARY_PATH=$EXTRACTED_PREFIX/lib:$LD_LIBRARY_PATH
+
+linuxdeployqt "$DT_PREFIX/share/applications/dwarftherapist.desktop" -appimage -qmake=$QT_PREFIX/bin/qmake
+
+# Rename DT AppImage using current tag
+filename=Dwarf_Therapist-x86_64.AppImage
+mv -v "$filename" "${filename/Dwarf_Therapist/DwarfTherapist-${TRAVIS_TAG}-${TRAVIS_OS_NAME}}"
+
