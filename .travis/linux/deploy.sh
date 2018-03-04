aea89a092c533e7d107a4443c9a1ba27c9eb3365
Clément Vuchener <clement.vuchener@gmail.com>
2018-03-04 11:45:03 +0100

    travis: Bundle libstdc++ as optional library
    Fix AppImage on pre gcc-5 linux distributions.

--- .travis/linux/deploy.sh
+++ .travis/linux/deploy.sh
@@ -3,23 +3,34 @@
 ninja install
 
 # Add PDF manual
-install -m 0644 -D -t "$DT_PREFIX/share/doc/dwarftherapist/" "Dwarf Therapist.pdf"
+install -m 0644 -D -t "$APPDIR/usr/share/doc/dwarftherapist/" "Dwarf Therapist.pdf"
 
-# Download linuxdeployqt
+# Download and extract linuxdeployqt (because of missing FUSE)
 wget "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-linuxdeployqt="./linuxdeployqt-continuous-x86_64.AppImage"
-chmod +x "$linuxdeployqt"
+chmod +x linuxdeployqt*.AppImage
+./linuxdeployqt*.AppImage --appimage-extract
+mv squashfs-root linuxdeployqt
 
-# does not work because of missing FUSE
-#"$linuxdeployqt" "$PREFIX/usr/share/applications/dwarftherapist.desktop" -appimage
+linuxdeployqt/AppRun "$APPDIR/usr/share/applications/dwarftherapist.desktop" -bundle-non-qt-libs -qmake=$QT_PREFIX/bin/qmake
 
-# extract AppImage content instead
-"$linuxdeployqt" --appimage-extract
-EXTRACTED_PREFIX=$PWD/squashfs-root/usr
-export PATH=$EXTRACTED_PREFIX/bin:$PATH
-export LD_LIBRARY_PATH=$EXTRACTED_PREFIX/lib:$LD_LIBRARY_PATH
+# Download patched AppRun for optional loading of libstdc++
+wget "https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/AppRun-patched-x86_64"
+chmod +x "AppRun-patched-x86_64"
+rm "$APPDIR/AppRun"
+cp "AppRun-patched-x86_64" "$APPDIR/AppRun"
+mkdir -p "$APPDIR/usr/optional/"{libstdc++,libgcc_s}
+wget "https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/exec-x86_64.so"
+cp "exec-x86_64.so" "$APPDIR/usr/optional/exec.so"
+cp "/usr/lib/x86_64-linux-gnu/libstdc++.so.6" "$APPDIR/usr/optional/libstdc++/"
+cp "/lib/x86_64-linux-gnu/libgcc_s.so.1" "$APPDIR/usr/optional/libgcc_s/"
 
-linuxdeployqt "$DT_PREFIX/share/applications/dwarftherapist.desktop" -appimage -qmake=$QT_PREFIX/bin/qmake
+# Download and extract appimagetool
+wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod +x "appimagetool-x86_64.AppImage"
+./appimagetool-x86_64.AppImage --appimage-extract
+mv squashfs-root appimagetool
+
+appimagetool/AppRun "$APPDIR"
 
 # Rename DT AppImage using current tag
 filename=Dwarf_Therapist-x86_64.AppImage
