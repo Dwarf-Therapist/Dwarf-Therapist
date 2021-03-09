e537c9e0b4888ce6f43c08196713fdaed4618164
Clément Vuchener <clement.vuchener@gmail.com>
2021-03-09 18:40:57 +0100

    Fix macOS deployment target variable
    and remove useless 5.12 build

--- .travis/osx/install_deps.sh
+++ .travis/osx/install_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
 
+unset MACOSX_DEPLOYMENT_TARGET # pip3 doesn't like this variable, it's only needed for cmake anyway
 pip3 install aqtinstall
 aqt install -O "$HOME/Qt" "$QT_VERSION" mac desktop
